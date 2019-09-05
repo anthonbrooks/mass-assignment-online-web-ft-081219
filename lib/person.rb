a9ab8c991a7b2410dd43b6aa551a4b16a904c775
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-09-05 23:20:37 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,7 @@
 class Person
   
+  attr_accessible :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
+  
   def initialize(name:, birthday:, hair_color:, eye_color:, height:, weight:, handed:, complexion:, t_shirt_size:, wrist_size:, glove_size:, pant_length:, pant_width:) 
     
   end 
